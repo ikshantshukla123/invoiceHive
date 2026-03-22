275cf3afb4957b0c64d96d276b36fc743643a965
Ikshant Shukla <ikshankshukla44@gmail.com>
2026-03-22 22:31:21 +0530

    established mongodb connection

---
+++ auth-service/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "./env";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1); // Kill the service — nothing works without DB
+  }
+};
